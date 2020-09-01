f99114c012738c9e143f66f9c7fbbfc91c4ba2d6
YANGJIAKUN <200000725@163.com>
2020-09-01 18:13:54 +0800

    修改轮播图

--- packages/carousel/carousel.vue
+++ packages/carousel/carousel.vue
@@ -1,10 +1,23 @@
 <template>
     <div class="star-carousel">
-        <div class="star-carousel__box" ref="carouselBox" @touchstart="handlerStart" @touchmove="handlerMove" @touchend="handlerEnd">
+        <div
+            class="star-carousel__box"
+            ref="carouselBox"
+            @touchstart="handlerStart"
+            @touchmove="handlerMove"
+            @touchend="handlerEnd"
+        >
             <slot />
         </div>
         <div class="star-carousel__instructions">
-            <div :class="['star-carousel__instructions__box',currentActive == index && 'star-background']" v-for="(item , index) of childCount" :key="index"></div>
+            <div
+                :class="[
+                    'star-carousel__instructions__box',
+                    currentActive == index + 1 && 'star-background',
+                ]"
+                v-for="(item, index) of childCount"
+                :key="index"
+            ></div>
         </div>
     </div>
 </template>
@@ -13,82 +26,88 @@ export default {
     name: 'starCarousel',
     props: {
         duration: {
-            type: [String,Number],
-            default: 3000
+            type: [String, Number],
+            default: 3000,
         },
         autoPlay: {
             type: Boolean,
-            default: true
-        }
+            default: true,
+        },
     },
     data() {
         return {
             childCount: 0,
             carouselBox: null,
             timeName: null,
-            currentActive: 0,
+            currentActive: 1,
             start: 0,
-            move: 0
+            move: 0,
         }
     },
     mounted() {
-        this.$nextTick(()=>{
+        this.$nextTick(() => {
             this.carouselBox = this.$refs.carouselBox
             this.childCount = this.$children.length
-            if(this.autoPlay) {
+            if (this.autoPlay) {
                 this.setIntervalFun()
             }
         })
     },
     methods: {
-        // 当手指触摸开始
+        // 手指触碰时
         handlerStart(event) {
             clearInterval(this.timeName)
-            this.start = event.touches[0].pageX
+            this.start = event.touches[0].clientX
         },
-        // 当手指移动时
+        // 手指移动时
         handlerMove(event) {
-            let move = event.touches[0].pageX
-            this.move = this.start - event.touches[0].pageX
-            this.setTransfromFun(this.move)
+            let move = event.touches[0].clientX
+            this.move = this.start - move
+            this.setTransfromFun(this.move, false)
         },
-        // 当手指触摸结束
-        handlerEnd(event) {
-            console.log(this.move)
-            if(this.move > 100) {
-                this.currentActive ++
-            } else if(this.move < -100) {
-                this.currentActive --
+        // 手指触碰结束
+        handlerEnd() {
+            if (this.move > 100) {
+                this.currentActive++
+            } else if (this.move < -100) {
+                this.currentActive--
             }
-            this.setActiveFun()
-            this.setTransfromFun()
+            this.setAction()
+            this.setTransfromFun(0, true)
             this.setIntervalFun()
         },
+        // 设置定滚动间隔
         setIntervalFun() {
-            this.timeName = setInterval(()=>{
-                this.currentActive ++
-                this.setActiveFun()
-                this.setTransfromFun()
-            },this.duration)
+            this.timeName = setInterval(() => {
+                this.currentActive++
+                this.setAction()
+                this.setTransfromFun('', true)
+            }, this.duration)
         },
-        setActiveFun() {
-            if(this.currentActive >= this.childCount) {
-                this.currentActive = 0
-            } else if(this.currentActive < 0) {
-                this.currentActive = this.childCount - 1
+        // 设置下标
+        setAction() {
+            if (this.currentActive > this.childCount) {
+                this.currentActive = 1
+            } else if (this.currentActive < 1) {
+                this.currentActive = this.childCount
             }
         },
-        setTransfromFun(move){
-            if(!move) {
-                this.carouselBox.style.transition = 'all 300ms ease'
-                move = 0
+        // 设置位移距离
+        setTransfromFun(move, noTransition) {
+            // move: 手指滑动时的距离
+            // noTransition: 是否需要过渡效果
+            if (!move || move == '') move = 0
+            if (noTransition) {
+                this.carouselBox.style.transitionDuration = '300ms'
             } else {
-                this.carouselBox.style.transition = 'all 0ms ease'
+                this.carouselBox.style.transitionDuration = '0ms'
             }
-            let X = `translateX(-${this.currentActive * this.carouselBox.clientWidth + move}px)`
-            console.log(X)
+            let X = `translateX(-${(this.currentActive - 1) *
+                this.carouselBox.clientWidth +
+                move}px)`
+            X = X.replace(/\-\-/, '')
             this.carouselBox.style.transform = X
-        }
+        },
     },
 }
 </script>
@@ -120,7 +139,7 @@ export default {
             width: 7px;
             height: 7px;
             border-radius: 50%;
-            background: rgba(225,225,225,0.4);
+            background: rgba(225, 225, 225, 0.4);
             margin-right: 24px;
             transition: all 500ms ease;
         }
